469875bda579a4495ff3046a0c261ceafb407632
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-23 19:03:12 +0530

    commit

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Employee.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Employee.java
@@ -4,10 +4,9 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("hey there !!!");
 		System.out.println("123456");
-<<<<<<< HEAD
+
 		System.out.println("Shivam");
-=======
+
 		System.out.println("shatakshi!!");
->>>>>>> branch 'main' of https://github.com/SHIVAMRAJPUT7078/Real-Estate-Broker-Application.git
-	}
+}
 }
