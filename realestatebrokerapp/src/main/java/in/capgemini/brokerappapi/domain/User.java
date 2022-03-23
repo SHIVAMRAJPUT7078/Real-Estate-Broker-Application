a03dc843985ab859e0914e3b619e8cc10be008d0
saumya <saumyagupta093@gmail.com>
2022-03-23 16:12:57 +0530

    third commit

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/User.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/User.java
@@ -3,6 +3,6 @@ package in.capgemini.brokerappapi.domain;
 public class User {
 
 	public static void main(String[] args) {
-		System.out.println("welcome Project");
+		System.out.println("welcome Real-estate-project");
 	}
 }
