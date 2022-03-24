c84ed39cbfd14462eb8b84d70f6a8fa5b1b61a14
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-24 08:40:23 +0530

    create field custOrder and custCart

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Customer.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Customer.java
@@ -2,5 +2,12 @@ package in.capgemini.brokerappapi.domain;
 
 public class Customer {
 
+	private String custId;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private String email;
+	private Order custOrder;
+	private Cart custCart;
 	
 }
