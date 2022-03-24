ec9c4b826c508371f151f88603746155dfa57522
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-24 08:49:46 +0530

    add CustomerRepository interface in repository package

---
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package in.capgemini.brokerappapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import in.capgemini.brokerappapi.domain.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, String>{
+
+}
