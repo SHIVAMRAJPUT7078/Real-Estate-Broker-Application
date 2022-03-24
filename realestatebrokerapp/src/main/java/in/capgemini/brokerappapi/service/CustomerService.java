41ca5a5693480bda536b84d8edb93e17b5bb9a9a
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-24 08:47:32 +0530

    change in customerService

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/service/CustomerService.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/service/CustomerService.java
@@ -1,9 +1,6 @@
 package in.capgemini.brokerappapi.service;
 
-import org.springframework.data.repository.CrudRepository;
 
-import in.capgemini.brokerappapi.domain.Customer;
-
-public interface CustomerService extends CrudRepository<Customer, String> {
+public interface CustomerService {
 
 }
