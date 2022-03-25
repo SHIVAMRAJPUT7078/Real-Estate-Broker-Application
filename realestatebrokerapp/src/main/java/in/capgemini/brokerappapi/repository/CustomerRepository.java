8a3c5e0e09273ceddde82af7faf8fc732fef5e20
Satyam <Satyam@DESKTOP-69166PF>
2022-03-25 13:55:54 +0530

    commit

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/repository/CustomerRepository.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
-//package in.capgemini.brokerappapi.repository;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//import in.capgemini.brokerappapi.domain.Customer;
-//
-//public interface CustomerRepository extends CrudRepository<Customer, Long>{
-//
-//}
+package in.capgemini.brokerappapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import in.capgemini.brokerappapi.domain.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+}
--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/service/CustomerService.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/service/CustomerService.java
@@ -1,19 +1,19 @@
-//package in.capgemini.brokerappapi.service;
-//
-//import java.util.List;
-//
-//import in.capgemini.brokerappapi.domain.Customer;
-//
-///**
-// * @author hsraj
-// *
-// */
-//public interface CustomerService {
-//
-//	void addCustomer(Customer customer);
-//	void updateCustomer(Customer customer);
-//	void removeCustomer(Customer customer);
-//	Customer viewCustomer();
-//	List<Customer> viewAllCustomer();
-//	
-//}
+package in.capgemini.brokerappapi.service;
+
+import java.util.List;
+
+import in.capgemini.brokerappapi.domain.Customer;
+
+/**
+ * @author hsraj
+ *
+ */
+public interface CustomerService {
+
+	void addCustomer(Customer customer);
+	void updateCustomer(Customer customer);
+	void removeCustomer(Customer customer);
+	Customer viewCustomer();
+	List<Customer> viewAllCustomer();
+	
+}
