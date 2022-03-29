32284b3c3ae4443c85c30e7d801d4322e7d67070
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-29 10:26:41 +0530

    Customer completed

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Customer.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Customer.java
@@ -3,49 +3,123 @@ package in.capgemini.brokerappapi.domain;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import in.capgemini.brokerappapi.reauest.EmployeeRequest;
 
 
 /**
  * @author hsraj
  *
  */
+
+
 @Entity
 public class Customer {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long custId;
-	private String firstName;
-	private String lastName;
+	private Long id;
+	@NotBlank(message = "customerIdentifier is required")
+	@Column(updatable = false , unique = true)
+	private String customerIdentifier;
+	@NotBlank(message = "customerFirstName is required")
+	private String customerFirstName;
+	private String customerLastName;
+	private String email;
 	@OneToOne(fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_name")
 	private Address address;
-//	private String email;
+	
+	
+	public Customer() {
+		super();
+		
+	}
+
+
+	public Customer(EmployeeRequest employeeRequest) {
+		this.customerIdentifier = employeeRequest.getCustomerIdentifier();
+		this.customerFirstName = employeeRequest.getCustomerFirstName();
+		this.customerLastName = employeeRequest.getCustomerLastName();
+		this.email = employeeRequest.getEmail();
+
+	}
+//	@OneToOne(fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+//	private Address address;
+	
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCustomerIdentifier() {
+		return customerIdentifier;
+	}
+	public void setCustomerIdentifier(String customerIdentifier) {
+		this.customerIdentifier = customerIdentifier;
+	}
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+	public String getCustomerLastName() {
+		return customerLastName;
+	}
+	public void setCustomerLastName(String customerLastName) {
+		this.customerLastName = customerLastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 //	private List<Order> custOrder;
 //	private Cart custCart;
-	public Long getCustId() {
-		return custId;
-	}
-	public void setCustId(Long custId) {
-		this.custId = custId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+
 	/*
 	public Address getAddress() {
 		return address;
@@ -73,4 +147,10 @@ public class Customer {
 	}*/
 	
 	
-}
+
+
+
+
+
+
+
