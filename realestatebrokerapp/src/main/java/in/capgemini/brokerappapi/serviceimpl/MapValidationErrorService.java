9b36a7befc74908103733a97d4b7de1a276b705b
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-29 10:25:58 +0530

    add MapValidationErrror service

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/serviceimpl/MapValidationErrorService.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/serviceimpl/MapValidationErrorService.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+
+@Service
 public class MapValidationErrorService {
 	 public ResponseEntity<?> mapValidationError(BindingResult result){
 			if(result.hasErrors()) {
