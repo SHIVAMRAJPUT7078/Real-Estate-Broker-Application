27e500463581b2484c7903f60337566f28a1f3ae
saumya <saumyagupta093@gmail.com>
2022-03-29 10:10:13 +0530

    extra commit

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package in.capgemini.brokerappapi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import in.capgemini.brokerappapi.domain.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
-
-}
--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/web/OrderController.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/web/OrderController.java
@@ -1,9 +1,15 @@
 package in.capgemini.brokerappapi.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,30 +17,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import in.capgemini.brokerappapi.domain.Order1;
 import in.capgemini.brokerappapi.service.OrderService;
 import in.capgemini.brokerappapi.serviceimpl.MapValidationErrorService;
 
 @RestController
-@RequestMapping("api/orders")
-public class ProjectController {
+@RequestMapping("api/orderss")
+public class OrderController {
   
 	@Autowired
 	private OrderService orderService;
-	
-	public Order1 createNewOrder( @RequestBody Order1 order) {
-		Order1 savedOrder=orderService.saveorUpdate(order);
-		return savedOrder;
-	}
 	@Autowired
-	private MapValidationErrorService  mapValidationErrorService;
-	
+	private MapValidationErrorService mapValidationErrorService;
 	@PostMapping("")
-	public ResponseEntity<?> createNewOrder( @RequestBody Order1 order ,BindingResult result){
+	public ResponseEntity<?> createNewOrder(@Valid @RequestBody Order1 order,BindingResult result) {
 		ResponseEntity<?> errorMap=mapValidationErrorService.mapValidationError(result);
 		if(errorMap!=null) return errorMap;
-		
 		Order1 savedOrder=orderService.saveorUpdate(order);
 		return new ResponseEntity<Order1>(savedOrder,HttpStatus.CREATED);
 	}
@@ -51,9 +49,9 @@ public class ProjectController {
 		return orderService.findAllOrder();
 	}
 	
-	@DeleteMapping("/{orderid}")
+	@DeleteMapping("/{orderId}")
 	public ResponseEntity<?> deleteOrder(@PathVariable String orderId){
 		orderService.deleteOrderByOrderIdentifier(orderId);
-		return new ResponseEntity<String>("Order with Id:"+orderId.toUpperCase()+"Deleted successfully!",HttpStatus.OK);
+		return new ResponseEntity<String>("Order with Id: "+orderId.toUpperCase()+" Deleted successfully!",HttpStatus.OK);
 	}
 }
