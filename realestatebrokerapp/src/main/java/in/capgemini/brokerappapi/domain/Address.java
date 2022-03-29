bfc25e180a6d55cfafb8521cdf05d862b495b7e5
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-29 10:25:11 +0530

    add class Adress

--- realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Address.java
+++ realestatebrokerapp/src/main/java/in/capgemini/brokerappapi/domain/Address.java
@@ -3,30 +3,61 @@ package in.capgemini.brokerappapi.domain;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Address {
 
 	@Id
-	private String aid;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String houseNumber;
+	private String street;
 	private String pinCode;
-	private String locality;
 	private String state;
-	@OneToOne(fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+	@OneToOne(fetch = FetchType.EAGER,mappedBy = "address")
+	@JoinColumn(name = "customer_id",nullable = false)
+    @JsonIgnore
 	private Customer customer;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
 	public String getPinCode() {
 		return pinCode;
 	}
 	public void setPinCode(String pinCode) {
 		this.pinCode = pinCode;
-	}
-	public String getLocality() {
-		return locality;
-	}
-	public void setLocality(String locality) {
-		this.locality = locality;
 	}
 	public String getState() {
 		return state;
@@ -34,5 +65,7 @@ public class Address {
 	public void setState(String state) {
 		this.state = state;
 	}
+	
+	
 	
 }
